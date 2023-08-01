42af4aa10f66aa84d537a978871b0dc30925b28b
caicara <joaovictor.dossantos-ext@digitalcode.com.br>
2023-08-01 00:42:05 -0300

    fix: eslint

--- src/components/svg-components/brand-login.tsx
+++ src/components/svg-components/brand-login.tsx
@@ -29,9 +29,9 @@ export const BrandLogin: FC<{ className?: string }> = ({ className }) => {
           width="228"
           height="69"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
