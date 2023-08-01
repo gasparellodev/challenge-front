d0b5dbd5b1afafb39ce0ea52d08aec65028e9410
caicara <joaovictor.dossantos-ext@digitalcode.com.br>
2023-08-01 05:01:33 -0300

    feat: header

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="bg-[#222429]">
         <Navigation />
         {children}
       </body>
--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,5 +1,13 @@
+"use client";
+import { Button } from "@/components/buttons";
 import { CourseCard } from "@/components/class-card";
+import { Search } from "@/components/search";
+import { MultipleSelect } from "@/components/select";
+import { ListIcon } from "@/components/svg-components/list-icon";
+import { ListMiniature } from "@/components/svg-components/list-miniature";
+import { PlusIcon } from "@/components/svg-components/plus-icon";
 import Image from "next/image";
+import { useState } from "react";
 
 const dataMock = [
   {
@@ -13,12 +21,60 @@ const dataMock = [
   },
 ];
 
+const selectMock = ["Produtividade"];
+
 export default function Home() {
+  const [listMiniature, setListMiniature] = useState(true);
+
+  const handleListView = () => {
+    setListMiniature(!listMiniature);
+  };
+
   return (
-    <section className="flex min-h-screen flex-col items-center justify-between p-24">
+    <section className="flex h-full flex-col items-center justify-between py-20">
+      {/* HEAD */}
+      <div className="h-[76px] flex flex-row w-full bg-[#2A2E39] px-[20px] justify-around items-center">
+        <div className="flex flex-row">
+          <h2 className="font-[700] text-[22px]">Meus cursos</h2>
+        </div>
+
+        <div className="flex flex-row items-center h-full">
+          <p className="font-[700] text-secundary text-[10px] mr-[21px]">
+            FILTRAR POR CATEGORIA:{" "}
+          </p>
+          <MultipleSelect options={selectMock} />
+        </div>
+
+        <div>
+          <Search />
+        </div>
+
+        <div className="flex flex-row gap-3">
+          <p>VISUALIZAÇÃO</p>
+          <button onClick={handleListView}>
+            <ListMiniature color={listMiniature ? "#93BC1E" : "#646D85"} />
+          </button>
+
+          <button onClick={handleListView}>
+            <ListIcon color={!listMiniature ? "#93BC1E" : "#646D85"} />
+          </button>
+        </div>
+
+        <div>
+          <Button
+            iconDirection="left"
+            icon={<PlusIcon color="white" />}
+            label="Novo curso"
+            className="text-[14px] w-[128px] h-[40px]"
+          />
+        </div>
+      </div>
+      {/* CARDS */}
       {dataMock.map((data) => {
         return <CourseCard course={data} />;
       })}
+
+      {/* FOOTER */}
     </section>
   );
 }
