ba5a753a31b0559b6b0721a81a71b0a015b66700
caicara <joaovictor.dossantos-ext@digitalcode.com.br>
2023-08-01 00:43:36 -0300

    feat: apply course card component

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,10 +1,24 @@
+import { CourseCard } from "@/components/class-card";
 import Image from "next/image";
+
+const dataMock = [
+  {
+    course_name: "Javascript",
+    course_description: "Lorem ipsum lore",
+    course_type: "Link externo",
+    course_category: "Produtividade",
+    course_link: "http://lorem.com.br",
+    link_support: "https://lorem.help.com.br",
+    course_image: null,
+  },
+];
 
 export default function Home() {
   return (
     <section className="flex min-h-screen flex-col items-center justify-between p-24">
-
-      
+      {dataMock.map((data) => {
+        return <CourseCard course={data} />;
+      })}
     </section>
   );
 }
